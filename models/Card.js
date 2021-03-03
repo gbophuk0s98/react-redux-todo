2984628ad3acefcc6872ad3f8fcfa213fce1188e
Surta <pasha.surta@mail.ru>
2021-03-03 15:38:36 +0300

    Изменил модель карточек. Переношу роуты для тудушек и карточек в отдельный файл

--- models/Card.js
+++ models/Card.js
@@ -1,9 +1,10 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model, Types } = require('mongoose')
 
 const schema = new Schema({
     name: { type: String, required: true},
     columnType: { type: String, required: true },
-    items: { type: Array, required: false }
+    items: { type: Array, required: false },
+    project: { type: Types.ObjectId, ref: 'projects'}
 })
 
 module.exports =  model('cards', schema)
--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -2,40 +2,95 @@ const { Router } = require('express')
 const Todo = require('../models/Todo')
 const Card = require('../models/Card')
 const router = Router()
+const uuid = require('uuid')
 
-router.get('/todos', async (req, res) => {
-    
-})
+const updateCards = async (cards) => {
 
-router.post('/createCard', async (req, res) => {
+    const newCards = []
+
+    for (const card of cards) {
+        const { id, columnType, name } = card
+        const data = await Todo.find({ owner: card.id })
+        newCards.push({ id: id, name: name, columnType: columnType, items: [...data] })
+    }
+    return newCards
+}
+
+router.get('/getCards', async (req, res) => {
     try
     {
-        const { title } = req.body
-
-        const card = new Card({ title: title })
-        await card.save()
-
-        res.status(201).json({ message: 'Карточка создана' })
+        const cards = await Card.find()
+        const newCards = await updateCards(cards)
+        return res.status(200).json(cards)
     }
     catch (e)
     {
-        res.status(400).json({ message: 'Что-то пошло не так' })
+        res.status(500).json({ message: 'Внутренняя ошибка сервера', devMessage: `${e.message}` })   
+    }
+})
+
+router.get('/getTodos', async (req, res) => {
+    try
+    {
+        const todos = await Todo.find()
+        return res.status(200).json(todos)
+
+    }
+    catch (e)
+    {
+        res.status(500).json({ message: 'Внутренняя ошибка сервера', devMessage: `${e.message}` })
     }
 })
 
 router.post('/createTodo', async (req, res) => {
     try
     {
-        const { name, id } = req.body
-
-        const todo = new Todo({ name: name, id: id })
+        const { content, startDate, endDate } = req.body
+        const { _id, items } = await Card.findOne({ columnType: 'TaskList'})
+        const customId = uuid.v4()
+        await Card.updateOne({ columnType: 'TaskList' }, { $push: {items : { customId: customId, content: content, startDate: startDate, endDate: endDate, posNumber: items.length} }})
+        const todo = new Todo({ customId: customId, content: content, startDate: startDate, endDate: endDate, owner: _id })
         await todo.save()
+        return res.status(200).json({ message: 'Успешно создана!'})
     }
     catch (e)
     {
-        res.status(400).json({ message: 'Что-то пошло не так' })
+        res.status(500).json({ message: 'Внутренняя ошибка сервера', devMessage: `${e.message}` })
     }
 })
 
+router.put('/updateTodo', async (req, res) => {
+    try
+    {
+        let updateItem = {}
+        
+        if (req.body.hasOwnProperty('startDate')) updateItem = { startDate: req.body.startDate}
+        else if (req.body.hasOwnProperty('endDate')) updateItem = { endDate: req.body.endDate }
+
+        await Todo.updateOne({ _id: req.body.id }, updateItem, { upset: false })
+        return res.status(200).json({ message: 'Задача обновлена!'})
+    }
+    catch (e)
+    {
+        res.status(500).json({ message: 'Внутренняя ошибка сервера', devMessage: `${e.message}` })
+    }
+})
+
+router.post('/saveCards', async (req, res) => {
+    try
+    {
+        if (req.body) {
+            for (const key in req.body) {
+                await Card.updateOne({ _id: req.body[key]._id }, { items: req.body[key].items })
+            }
+        }
+
+        return res.status(200).json({ message: 'Успешно обновлены!' })
+    }
+    catch (e)
+    {
+        res.status(500).json({ message: 'Внутренняя ошибка сервера', devMessage: `${e.message}` })
+    }
+})
 
 module.exports = router
